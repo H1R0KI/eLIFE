83fc925955ae7b8088004829a1345e1841ae224d
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-04-04 11:29:21 +0000

    [add]bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+
+  #管理者側
   devise_for :admin,  skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 
+  #会員側
   devise_for :members, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+  scope module: :public do
+    #homes
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
