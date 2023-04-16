e2e72d0dfb5676561d1c33c2ba92e4964288ccdd
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-04-16 09:30:03 +0000

    [add]TOPランキング追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,8 @@
 class Public::HomesController < ApplicationController
+
   def top
+    @week_post_favorite_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
+    @member_post_ranks = Member.where(id: Post.group(:member_id).order('count(member_id) desc').limit(5).pluck(:member_id))
   end
 
   def about
