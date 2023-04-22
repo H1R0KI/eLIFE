f1ac6163e65eb3146352294d3a571c04f5918ed3
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-04-22 11:27:25 +0000

    [update]レイアウト

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,8 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @week_post_favorite_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
-    @member_post_ranks = Member.where(id: Post.group(:member_id).order('count(member_id) desc').limit(5).pluck(:member_id))
+    @week_post_favorite_ranks = Post.find(Favorite.group(:post_id).where(created_at: Time.current.all_week).order('count(post_id) desc').limit(5).pluck(:post_id))
+    @month_member_post_ranks = Member.find(Post.group(:member_id).where(created_at: Time.current.all_month).order('count(member_id) desc').limit(5).pluck(:member_id))
   end
 
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -23,14 +23,12 @@ class Public::PostsController < ApplicationController
   def index
     @posts = Post.all.page(params[:page]).per(5)
     @tags = Tag.all
-
   end
 
   def show
     @post = Post.find(params[:id])
     @post_tags = @post.tags
     @post_comment = PostComment.new
-
   end
 
   def edit
